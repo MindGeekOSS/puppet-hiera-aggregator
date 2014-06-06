9ad107bd3bb0b36e42a4c7089129242a9221232b
Alain Lefebvre <alain.lefebvre@mindgeek.com>
2014-06-06 11:46:42 -0400

    Various updates, including usage of json config file for parameters

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 puppet-hiera-aggregator
 =======================
+A python class to extract the config parameters from a hiera hierarchy for a given puppet agent.
 
-A python class to extract the config parameters from a hiera hierarchy for a given puppet agent
+##Description
+=======================
+
+In some cases, you may have multiple levels of Hiera hierarchy for your puppet nodes.  This tool allows you to easily find out which level of the hierarchy your node is getting config data.  The end result is a final config which would be the end result for a puppet run.
+
+
+##Python Dependencies:
+=======================
+- paramiko
+- urllib2 
+- yaml
+
+
+##Config File:
+=======================
+
+- Rename the config file to config.json and set the appropriate values
+
+
+##Usage:
+=======================
+
+`python hiera-visualize.py [SERVER_FQDN]`
+
+- Note that the **[SERVER_FQDN]** parameter is optional. If specified, only the report for that host will be compiled.
+- A report is written to file for each node containing the final merged Hiera config
--- hiera-aggregator.py
+++ hiera-aggregator.py
@@ -2,41 +2,53 @@
 
 #Author: Alain Lefebvre <alain.lefebvre@mindgeek.com>     
 
-import sys, subprocess, json, paramiko, os, argparse, time, urllib2, yaml, codecs, re
+import sys, subprocess, json, paramiko, os, time, urllib2, yaml, codecs, re
 import pprint as pp
 
 
-class HieraVisualizer:
+class HieraAggregator:
 
 	config = {}
 	puppet_api_uri = {}
 	hierarchy = {}
+	_ssh_connection = None
 
 	def __init__(self):
 
 		self.puppet_api_uri = {
 			'node_list': '/v3/nodes',
 			'node_facts': '/v3/nodes/{0}/facts',
-			'fact_list': '/v3/fact-names',
 		}
 
 
 	def set_config(self, conf):
 
-		self.config['base_api_facts'] = conf['base_api_facts']
-		if self.config['base_api_facts'].endswith('/'):
-			self.config['base_api_facts'] = self.config['base_api_facts'][:-1]
+		# If only a config filename is passed and the file exists, load it as the config
+		if isinstance(conf, str):
+			if not os.path.isfile(conf):
+				print "Config file {0} not found!".format(conf)
+				sys.exit(0)
+			else:
+				with open(conf, 'r') as content_file:
+	    				self.config = json.loads(content_file.read())
 
-		if 'use_ssh' in conf and int(conf['use_ssh']) == 1:
-			self.config['use_ssh'] = 1
-			self.config['private_key_path'] = conf['private_key_path']
-			self.config['username'] = conf['username']
+		# Otherwise, use the specific config dict that was passed
 		else:
-			self.config['use_ssh'] = 0	
+			self.config['puppetdb_api'] = conf['puppetdb_api']
+			if 'use_ssh' in conf and int(conf['use_ssh']) == 1:
+				self.config['use_ssh'] = 1
+				self.config['private_key_path'] = conf['private_key_path']
+				self.config['username'] = conf['username']
+			else:
+				self.config['use_ssh'] = 0	
 
-		self.config['puppet_hostname'] = conf['puppet_hostname']
-		self.config['hiera_local_file_dir'] = conf['hiera_local_file_dir']
-		self.config['hiera_config'] = conf['hiera_config']
+			self.config['puppet_hostname'] = conf['puppet_hostname']
+			self.config['hiera_local_file_dir'] = conf['hiera_local_file_dir']
+			self.config['hiera_config'] = conf['hiera_config']
+
+		# Remove the trailing slash if it exists
+		if self.config['puppetdb_api'].endswith('/'):
+				self.config['puppetdb_api'] = self.config['puppetdb_api'][:-1]
 
 		# Extract the Hiera hierarchy
 		self.compute_hierarchy(self.config['hiera_config'])
@@ -47,19 +59,21 @@ class HieraVisualizer:
 
 		# Set the proper puppetdb API URI
 		if params['query_type'] == 'node_list':
-			facter_query_url = self.config['base_api_facts']+self.puppet_api_uri[params['query_type']]
+			facter_query_url = self.config['puppetdb_api']+self.puppet_api_uri[params['query_type']]
 
 		elif params['query_type'] == 'node_facts':
-			facter_query_url = self.config['base_api_facts']+self.puppet_api_uri[params['query_type']].format(params['fqdn'])
+			facter_query_url = self.config['puppetdb_api']+self.puppet_api_uri[params['query_type']].format(params['fqdn'])
 
 		# Now fetch the data either via SSH or HTTP		
 		if int(self.config['use_ssh']) == 1:
-			ssh = paramiko.SSHClient()
-			ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-			rsa_key = paramiko.RSAKey.from_private_key_file(self.config['private_key_path'])
-			ssh.connect(self.config['puppet_hostname'], username=self.config['username'], pkey=rsa_key)
 
-			stdin, stdout, stderr = ssh.exec_command("curl -X GET {0}".format(facter_query_url))
+			if self._ssh_connection == None:
+				self._ssh_connection = paramiko.SSHClient()
+				self._ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+				rsa_key = paramiko.RSAKey.from_private_key_file(self.config['private_key_path'])
+				self._ssh_connection.connect(self.config['puppet_hostname'], username=self.config['username'], pkey=rsa_key)
+
+			stdin, stdout, stderr = self._ssh_connection.exec_command("curl -X GET {0}".format(facter_query_url))
 			data = stdout.read()
 			json_data = json.loads(data)
 		else:
@@ -72,6 +86,12 @@ class HieraVisualizer:
 			tmp = {}
 			for e in json_data:
 				tmp[e['name']] = e['value']	
+			json_data = tmp		
+
+		elif params['query_type'] == 'node_list':
+			tmp = []
+			for e in json_data:
+				tmp.append(e['name'])
 			json_data = tmp		
 		
 		return json_data
@@ -109,18 +129,15 @@ class HieraVisualizer:
 
 		for lvl in self.hierarchy:
 
-			#print "Level:",lvl
-			
 			if 'facts' in lvl:
 				fact_vals = []
 				for f in lvl['facts']:
-					fact_vals.append(facts[f])
+					if f in facts:
+						fact_vals.append(facts[f])
 
 				hiera_file = '{0}/{1}.json'.format(lvl['group'], '-'.join(fact_vals))  
 			else:
 				hiera_file = '{0}.json'.format(lvl['group'])  
-
-			#print "File:",hiera_file
 
 			full_path = "{0}/{1}".format(file_path, hiera_file) 	
 			if os.path.isfile(full_path):
@@ -140,66 +157,77 @@ class HieraVisualizer:
 			return (order, properties)
 
 
-	def show_hierarchy(self, prop_order, properties):
 
-		lvl = 1
-		out = []
-		merged_properties = {}
-
-		for k in prop_order:
-			tabs = u'\t'*(lvl-1)
-			out.append(u"{0}\u21B3 Level {1} = {2}".format(tabs, lvl, prop_order[lvl-1]))
-			if merged_properties == {}:
-				merged_properties = properties[prop_order[lvl-1]];
-			else:
-				merged_properties = dict(merged_properties, **properties[prop_order[lvl-1]])
-			lvl+=1
+	def show_hierarchy_multi(self, results):
 
 
-		out.append("\nTotal Hiera files used: {0}\n".format(len(prop_order)))
-		out = '\n'.join(out)+"\n"
+		# was (prop_order, properties)
+		for i in results:
+			lvl = 1
+			out = []
+			prop_order, properties = results[i]
 
-		file_name = "hv-report.txt"
-		fo = codecs.open(file_name, "wb", "utf-8")
-		fo.write(out);
-		pp.pprint(json.dumps(merged_properties), stream=fo)		
-		fo.close()
-		
-		print out
-		print "\nResults saved to {0}\n".format(file_name)	
+			print "Host:", i
 
+			for k in prop_order:
+				tabs = u'\t'*(lvl-1)
+				out.append(u"{0}\u21B3 Level {1} = {2}".format(tabs, lvl, prop_order[lvl-1]))
+				lvl+=1
+
+			print '\n'.join(out)
+
+			file_name = "hv-report_{0}.txt".format(i)
+			fo = codecs.open(file_name, "wb", "utf-8")
+			fo.write(json.dumps(properties, indent=4, sort_keys=True))	
+			fo.close()
+
+			print "\nResults saved to {0}\n".format(file_name)	
+			print "-"*40
 		
 
 if __name__ == "__main__":
 
-	if len(sys.argv) == 1 or (len(sys.argv) >= 2 and sys.argv[1] == '-h'):
+
+	if len(sys.argv) >= 2 and sys.argv[1] == '-h':
 		print "Usage: ./hiera-visualize [SERVER_FQDN]\n"
 		sys.exit(0)
 
 	print ""
 
-	hv = HieraVisualizer()
-	hv.set_config(
-		{
-			'use_ssh': 1,
-			'base_api_facts': 'http://localhost:8080/',
-			'private_key_path': '[PRIVATE_KEY_PATH]',
-			'username': '[USERNAME]',
-			'puppet_hostname': '[PUPPET_MASTER_HOSTNAME]',
-			'hiera_local_file_dir': '[LOCAL_HIERA_DIR]',
-			'hiera_config': '[LOCAL_HIERA_CONFIG]'
-		}
-	)
+	hv = HieraAggregator()
+	hv.set_config('config.json')
 
 	# Get the list of nodes on the given puppet master
-	#nl = hv.query_facter({'query_type': 'node_list'})
+	if len(sys.argv) == 1:
+		
+		result = {}
+		print "NOTICE: fetching node list from puppet master {0}..".format(hv.config['puppet_hostname'])
+		nl = hv.query_facter({'query_type': 'node_list'})
+		total_nodes = len(nl)
+		print "NOTICE: {} nodes total".format(total_nodes)
+		print "NOTICE: fetching individual node facts.."
+		i=1
 
-	# Collect all the node facts for the specific host 
-	facts = hv.query_facter({'query_type': 'node_facts', 'fqdn': sys.argv[1]})
+		for node in nl:
+			# Get the node facts
+			facts = hv.query_facter({'query_type': 'node_facts', 'fqdn': node})
+			# Build a final merged list of all the config data to be applied on this host
+			chu = hv.build_config_hierarchy(facts, True)
+			result[node] = chu
+			sys.stdout.write("\r%d of %d nodes complete" % (i, total_nodes))
+    			sys.stdout.flush()
+			i+=1
 
-	# Build a final merged list of all the config data to be applied on this host
-	chu = hv.build_config_hierarchy(facts, True)
-	pp.pprint(chu[1])
+		print "-"*40
+
+		hv.show_hierarchy_multi(result)
+
+	else:
+		print "NOTICE: fetching node facts for {}..".format(sys.argv[1])
+		facts = hv.query_facter({'query_type': 'node_facts', 'fqdn': sys.argv[1]})	
+		# Build a final merged list of all the config data to be applied on this host
+		chu = hv.build_config_hierarchy(facts, True)
+		hv.show_hierarchy_multi({sys.argv[1]: chu})
 
 	
 
